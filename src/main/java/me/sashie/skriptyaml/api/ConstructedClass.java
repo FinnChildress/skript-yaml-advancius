3f5e76045160a9ab5bdbc1fe9309e6a2ff572f73
Rebecca <60572595+RagbeccaPlebs@users.noreply.github.com>
2022-10-30 10:01:57 +0100

    Fixed Version

---
+++ src/main/java/me/sashie/skriptyaml/api/ConstructedClass.java
@@ -0,0 +1,31 @@
+package me.sashie.skriptyaml.api;
+
+import java.util.Map;
+
+import me.sashie.skriptyaml.SkriptYaml;
+import org.yaml.snakeyaml.constructor.AbstractConstruct;
+import org.yaml.snakeyaml.nodes.MappingNode;
+import org.yaml.snakeyaml.nodes.Node;
+
+/**
+ * Needed to register a custom tag.
+ * 
+ * @param <T>
+ */
+public abstract class ConstructedClass<T> extends AbstractConstruct {
+
+	/**
+	 * Prepare a value for deserialization.
+	 * 
+	 * @param values
+	 *            the serialized map
+	 * @return the deserialized data
+	 */
+	public abstract T construct(Map<Object, Object> values);
+	
+	@Override
+	public Object construct(Node node) {
+		final Map<Object, Object> values = SkriptYaml.getInstance().getConstructor().constructMap((MappingNode) node);
+		return construct(values);
+	}
+}
